26f72443c42f6c2d1fe39c50d4d83e9253ccaa04
Vimukthi Fernando <vimukthishenalfdo@gmail.com>
2025-10-26 21:40:34 +0530

    created promises easy way to use

---
+++ promisesEasy.js
@@ -0,0 +1,18 @@
+const {readFile,writeFile} = require('fs').promises
+
+const start = async()=>{
+
+    try{
+        const first = await readFile('./files/first.txt','utf8')
+        const second = await readFile('./files/second.txt','utf8')
+        
+        await writeFile('./files/newWriteFile.txt',`The text is like this ${first} and ${second}`)
+
+    }catch(e){
+        console.log(e)
+    }
+
+}
+
+start()
+
